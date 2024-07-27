309ba5eeb466bdf28f3255d7e0eb679571440b7f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-27 21:04:39 +0800

    ad blank r project files

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=PathwayAssembler
+
+declare namespace PathwayAssembler {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
