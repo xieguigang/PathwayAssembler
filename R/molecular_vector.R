e2e486e76439010845d257207c08c9d43704b094
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-27 21:08:47 +0800

    add script file

---
+++ R/molecular_vector.R
@@ -0,0 +1,12 @@
+#' parse the structure of molecular and then convert to atom link vector
+#' 
+#' @param refmet a dataframe object of the reference metabolite, should 
+#'     contains the ``refmet_name`` of the metabolite as identifier and 
+#'     the ``smiles`` structure data for do molecular vector embedding.
+#' 
+#' @return a tuple list of the atom link vector data of each reference 
+#'     metabolite which have the smiles structure data.
+#' 
+const molecular_vector = function(refmet) {
+
+}
