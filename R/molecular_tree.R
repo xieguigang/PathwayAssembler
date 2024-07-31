f2db6fbfc262182b3bf208baf74f99f011a0f375
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-31 23:07:52 +0800

    test embedding

--- R/molecular_tree.R
+++ R/molecular_tree.R
@@ -1,8 +1,17 @@
+require(umap);
+
 #' Make metabolite clustering based on their structures similarity
 #' 
 #' @param refmet the vector embedding result of the reference metabolites, which is 
 #'    parsed via the ``molecular_vector`` function.
 #'  
 const molecular_tree = function(refmet) {
+    let [labels, umap] = umap(refmet, dimension = 10);
+    refmet = as.data.frame(umap, labels = labels);
 
+    # view of the vector embedding result
+    print(refmet);
+
+    # make clusters
+    
 }
