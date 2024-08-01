4fecf2ed608db41074d83aa6610c701cb0a05f6f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-01 17:51:59 +0800

    define similarity score for metabolite structure vector

---
+++ src/PathwayBuilder/Builder.vb
@@ -0,0 +1,13 @@
+﻿Imports std = System.Math
+
+Module Builder
+
+    Public Function Similarity(a As Double(), b As Double()) As Double
+        For i As Integer = 0 To a.Length - 1
+            a(i) = std.Min(a(i), b(i)) / std.Max(a(i), b(i))
+        Next
+
+        Return a.Average
+    End Function
+
+End Module
