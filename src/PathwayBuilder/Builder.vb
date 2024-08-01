0051055dbd0952b49331cd067164c04330646f48
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-01 17:52:59 +0800

    fix score for zero/zero

--- src/PathwayBuilder/Builder.vb
+++ src/PathwayBuilder/Builder.vb
@@ -3,8 +3,16 @@
 Module Builder
 
     Public Function Similarity(a As Double(), b As Double()) As Double
+        Dim vi As Double
+
         For i As Integer = 0 To a.Length - 1
-            a(i) = std.Min(a(i), b(i)) / std.Max(a(i), b(i))
+            vi = std.Min(a(i), b(i))
+
+            If vi = 0.0 Then
+                a(i) = 0
+            Else
+                a(i) = vi / std.Max(a(i), b(i))
+            End If
         Next
 
         Return a.Average
