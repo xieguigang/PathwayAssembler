49dd402a15cf99ed7c41b0c5468d2fc3c096eca9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-01 20:32:14 +0800

    export api for build small network

--- src/PathwayBuilder/Builder.vb
+++ src/PathwayBuilder/Builder.vb
@@ -1,8 +1,15 @@
-﻿Imports std = System.Math
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Math.DataFrame
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Vectorization
+Imports rdataframe = SMRUCC.Rsharp.Runtime.Internal.[Object].dataframe
+Imports std = System.Math
 
+<Package("Builder")>
 Module Builder
 
-    Public Function Similarity(a As Double(), b As Double()) As Double
+    Public Function Similarity(a As Double(), b As Double()) As (Double, Double)
         Dim vi As Double
 
         For i As Integer = 0 To a.Length - 1
@@ -15,7 +22,22 @@ Module Builder
             End If
         Next
 
-        Return a.Average
+        Return (a.Average, 0)
     End Function
 
+    <ExportAPI("small_network")>
+    Public Function SmallNetwork(x As rdataframe) As DataMatrix
+        Dim metabolites = x.forEachRow() _
+            .Select(Function(v)
+                        Return New NamedCollection(Of Double)(v.name, CLRVector.asNumeric(v.value))
+                    End Function) _
+            .ToArray
+        Dim mat = metabolites.MatrixBuilder(
+            vector:=Function(v) v.value,
+            eval:=AddressOf Similarity,
+            type:=DataType.Similarity
+        )
+
+        Return mat
+    End Function
 End Module
