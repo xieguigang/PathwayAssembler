05f7d6b4f03bb7ff6238733a5439558ef8542a4b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-01 12:44:26 +0800

    export matrix

--- R/molecular_tree.R
+++ R/molecular_tree.R
@@ -5,12 +5,13 @@ require(umap);
 #' @param refmet the vector embedding result of the reference metabolites, which is 
 #'    parsed via the ``molecular_vector`` function.
 #'  
-const molecular_tree = function(refmet) {
+const molecular_tree = function(refmet, workdir = "./") {
     let [labels, umap] = umap(refmet, dimension = 10);
     refmet = as.data.frame(umap, labels = labels);
 
     # view of the vector embedding result
     print(refmet);
+    write.csv(refmet, file = file.path(workdir, "umap_embedding.csv"));
 
     # make clusters
     
--- test/vec_tree.R
+++ test/vec_tree.R
@@ -1,3 +1,3 @@
 require(PathwayAssembler);
 
-molecular_tree(refmet = read.csv(file.path(@dir, "atoms_vec.csv"), row.names = 1, check.names = FALSE));
+molecular_tree(refmet = read.csv(file.path(@dir, "atoms_vec.csv"), row.names = 1, check.names = FALSE), @dir);
