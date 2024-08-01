3ce4c90b46bce9fbe0b207592988495fbf9e83d5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-01 17:13:29 +0800

    make alignment score

--- R/molecular_tree.R
+++ R/molecular_tree.R
@@ -6,13 +6,30 @@ require(umap);
 #'    parsed via the ``molecular_vector`` function.
 #'  
 const molecular_tree = function(refmet, workdir = "./") {
+
+
+    # make clusters
+    
+}
+
+const embedding_vec = function(refmet, workdir = "./") {
     let [labels, umap] = umap(refmet, dimension = 10);
     refmet = as.data.frame(umap, labels = labels);
 
     # view of the vector embedding result
     print(refmet);
     write.csv(refmet, file = file.path(workdir, "umap_embedding.csv"));
+}
 
-    # make clusters
-    
+const align_score = function(a, b) {
+    append(names(a), names(b)) 
+    |> unique() 
+    |> sapply(i => {
+        let ai = ifelse(i in a, a[[i]], 0);
+        let bi = ifelse(i in b, b[[i]], 0);
+
+        min(ai,bi)/max(ai,bi);
+    })
+    |> mean()
+    ;
 }
