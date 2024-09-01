aa4364c931bd42b74829291ba7b2014ba7dfa81c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-01 19:25:02 +0800

    make updates of the structue dataset

--- test/smiles_vec_test.R
+++ test/smiles_vec_test.R
@@ -2,4 +2,5 @@ require(PathwayAssembler);
 
 # atoms = molecular_vector(refmet(), workdir = @dir);
 
+# create the molecule strcutre dataset
 write.csv(molecular_vector(refmet(), workdir = @dir), file  = file.path(@dir, "atoms_vec.csv"));
