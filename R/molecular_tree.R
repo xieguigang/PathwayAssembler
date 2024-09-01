d6eec766e4981024f45504b1c70e715ea759156c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-02 00:01:24 +0800

    make updates of the network graph

--- R/molecular_tree.R
+++ R/molecular_tree.R
@@ -2,18 +2,23 @@ require(umap);
 require(igraph.builder);
 require(igraph);
 
+imports "visualizer" from "igraph";
+
 #' Make metabolite clustering via UMAP algorithm
 #' 
 #' @param refmet the sgt vector embedding result of the reference metabolites, which is 
 #'    parsed via the ``molecular_vector`` and ``embedding_vec`` function.
 #'  
 const molecular_tree = function(refmet) {
-    let [umap, labels] = umap(refmet, dimension = 3);
+    let [umap, labels] = umap(refmet, dimension = 6,numberOfNeighbors = 64,
+            spectral_cos= TRUE);
     let embedding = as.data.frame(umap, labels = labels);
     let g = umap::as.graph(umap,
                            labels = labels,
-                           threshold = 0,
+                           threshold = 0.1,
                         make_abstract = FALSE);
+
+    g <- setColors(g, color = "dotnet.colors");
 
     list(embedding, graph = g);
 }
--- test/04_vec_tree.R
+++ test/04_vec_tree.R
@@ -9,4 +9,4 @@ let [embedding, graph] = molecular_tree(refmet = vec);
 
 write.csv(embedding, file = "./umap_embedding.csv");
 
-igraph::save.network(graph, file = "./molecular_tree");
+igraph::save.network(graph, file = "./molecular_tree", _3d = TRUE);
