7cab49cd09ca23de38d7aca1d88836785c4edec1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-02 08:56:49 +0800

    run package msbuild

--- R/molecular_tree.R
+++ R/molecular_tree.R
@@ -9,8 +9,8 @@ imports "visualizer" from "igraph";
 #' @param refmet the sgt vector embedding result of the reference metabolites, which is 
 #'    parsed via the ``molecular_vector`` and ``embedding_vec`` function.
 #'  
-const molecular_tree = function(refmet) {
-    let [umap, labels] = umap(refmet, dimension = 6,numberOfNeighbors = 64,
+const molecular_tree = function(refmet, dims = 9) {
+    let [umap, labels] = umap(refmet, dimension = dims,numberOfNeighbors = 64,
             spectral_cos= TRUE);
     let embedding = as.data.frame(umap, labels = labels);
     let g = umap::as.graph(umap,
